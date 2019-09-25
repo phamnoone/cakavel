9fcb0664cd7dc99e588cfa73eaf02b476bbbed18
= <=>
2019-09-25 22:05:38 +0700

    update login

--- app/controllers/example.php
+++ app/controllers/example.php
@@ -1,10 +1,5 @@
 <?php
-
-
 class Example extends Controller {
-
-
-
     /*
      * http://localhost/example
      */
@@ -47,41 +42,34 @@ class Example extends Controller {
         $this->view('dashboard/test_model', $data);
         $this->view('template/footer');
 
-
     }
 
     function login(){
-      // var_dump($this->method);
-      // echo '<br>';
-      // var_dump($this->data);
-
-      // echo'<br>';
+      $title= array(
+        "mess" =>""
+      );
       $mang = $this->data;
-      // var_dump($mang);
-      echo '<br>';
-      if(count($mang)==0){
-        
-      }else {
+      if (count($mang) == 0 ){
+
+      } else {
     
-        if($this->method === 'POST'){
+        if ($this->method === 'POST') {
           $this->model('Users');
-          $user = $this->Users->checkLogin($mang['username'],$mang['password']);
-          var_dump($user);
-          // login thanh cong hay khong
-          if($user){
-            var_dump($user); 
+          $user = $this->Users->adminators($mang['username'],$mang['password']);
+          // login thanh cong
+          if ($user) {
             echo "<script>alert('Login thanh cong')</script>";
           }else{
-            echo "<script>alert('Tài khoản mật khẩu không chính xác')</script>";
+            $title['mess'] = "Đăng nhập không thành công !";
           }
             
-        }else{
+        } else {
             
         }
       }
-        $this->view('template/footerlogin');
-        $this->view('main/login');
-        $this->view('template/headerlogin');
+        $this->view('template/login/footer');
+        $this->view('main/login',$title);
+        $this->view('template/login/header');
     }
 
 }
