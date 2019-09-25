480c78b714082c89e6349f4f4bbfe1db8df9dbba
= <=>
2019-09-25 17:25:05 +0700

    update UI login

--- app/views/template/headerlogin.php
+++ app/views/template/headerlogin.php
@@ -7,15 +7,17 @@
 <!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="/public/images/icons/favicon.ico"/>
 <!--===============================================================================================-->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="/public/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="/public/fonts/iconic/css/material-design-iconic-font.min.css">
 <!--===============================================================================================-->
+
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="/public/stylesheets/util.css">
 	<link rel="stylesheet" type="text/css" href="/public/stylesheets/main.css">
 <!--===============================================================================================-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
