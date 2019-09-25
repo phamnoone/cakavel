b5772258cfa58e36835bc66a4b89d10bd48e3139
= <=>
2019-09-25 18:02:33 +0700

    update UI login

--- app/views/template/headerlogin.php
+++ app/views/template/headerlogin.php
@@ -9,15 +9,10 @@
 <!--===============================================================================================-->
 
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/public/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="/public/fonts/iconic/css/material-design-iconic-font.min.css">
-<!--===============================================================================================-->
-
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="/public/stylesheets/util.css">
 	<link rel="stylesheet" type="text/css" href="/public/stylesheets/main.css">
 <!--===============================================================================================-->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 </head>
 <body>
