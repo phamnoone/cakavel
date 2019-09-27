b2bf1cb9003f0a0e84f1ecf7d0570eeea8e999ed
= <=>
2019-09-27 17:28:33 +0700

    update login new

--- app/views/main/login.php
+++ app/views/main/login.php
@@ -1,39 +1,37 @@
-<div class="limiter">
-		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
-			<div class="wrap-login100">
-				<form class="login100-form validate-form"  method="POST">
-					<span class="login100-form-logo">
-						<img src="/public/images/logo.png"  width="120%">
-					</span>
-					<span class="login100-form-title p-b-34 p-t-27">
-						Wellcome TLU
-					</span>
-					<div class="wrap-input100 validate-input" data-validate = "Enter username">
-						<input class="input100" type="text" name="username" required placeholder="Username">
-					
-					</div>
-
-					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" type="password" name="password" required placeholder="Password">
-						
-					</div>
-
-					<div class="contact100-form-checkbox">
-						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-						<label class="label-checkbox100" for="ckb1">
-							Remember me
-						</label>
-					</div>
-					<span class="login100-form-title mt-2 mb-4" style="font-size: 20px;">
-						<?php echo $message; ?>
-					</span>
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
-							Login
-						</button>
-					</div>	
-				</form>
-			</div>
-		</div>
-	</div>
-	<div id="dropDownSelect1"></div>
+ <div>
+    <a class="hiddenanchor" id="signup"></a>
+    <a class="hiddenanchor" id="signin"></a>
+    <div class="login_wrapper">
+      <div class="animate form login_form">
+        <div align="center">
+          <div class="separator">
+            <div>
+              <h1>WelCome TLU</h1>
+            </div>
+          </div>
+          <img src="../public/images/logo.png" width="50%" >
+        </div>
+        <section class="login_content">
+          <form method="POST">
+            <h1>Login Form</h1>
+            <div>
+              <input type="text" class="form-control" placeholder="username" name="username" required >
+            </div>
+            <div>
+              <input type="password" class="form-control" placeholder="password" name="password" required >
+            </div>
+            <div>
+             <button class="btn btn-light submit" style=" background: #fff;text-decoration: none;border: solid 1px #ccc;">Log in</button>
+              <a class="reset_pass" href="#">Lost your password?</a>
+            </div>
+            <div class="clearfix"></div>
+            <div class="separator">
+              <div>
+                <h1><?php echo $message; ?></h1>
+              </div>
+            </div>
+          </form>
+        </section>
+      </div>
+    </div>
+  </div>
