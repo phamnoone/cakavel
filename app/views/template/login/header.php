819f39037b4b47a4adf4469f3bb50137bdc1f955
= <=>
2019-09-27 17:28:33 +0700

    update login new

--- app/views/template/login/header.php
+++ app/views/template/login/header.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Đăng nhập</title>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="icon" type="image/png" href="/public/images/icons/favicon.ico"/>
-		<link rel="stylesheet" type="text/css" href="/public/stylesheets/util.css">
-		<link rel="stylesheet" type="text/css" href="/public/stylesheets/main.css">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	</head>
-	<body>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Đăng nhập</title>
+  <link rel="icon" type="image/png" href="/public/images/icons/favicon.ico"/>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link href="/public/stylesheets/animate.min.css" rel="stylesheet">
+  <link href="/public/stylesheets/custom.min.css" rel="stylesheet">
+</head>
+<body class="login">
