51a7bcda13693d98afd927fb517f7545db529af5
= <=>
2019-09-29 08:46:40 +0700

    update review login

--- app/controllers/dashboard.php
+++ app/controllers/dashboard.php
@@ -3,17 +3,13 @@ class Dashboard extends Controller{
     /*
      * http://localhost/
      */
-    function index () {
-
-    }
-
   	function admin () {
-      if (isset($_SESSION['token']) ) {
+      if (isset($_SESSION['tokenAdmin']) ) {
           $this->view('template/managers/header');
           $this->view('main/managers');
           $this->view('template/managers/footer');
       } else {
-          header("Location: /Managers/login");
+          header("Location: /managers/login");
       }
    
     }
--- app/controllers/managers.php
+++ app/controllers/managers.php
@@ -3,19 +3,15 @@ class Managers extends Controller{
     /*
      * http://localhost/
      */
-    function index () {
-      $this->login();
-    }
-
   	function login () {
       $message = '';
         if ($this->method === 'POST') {
             $userAdmin = $this->data;
             $this->model('AdministratorsModel');
             if ($this->AdministratorsModel->checkLogin($userAdmin['username'], $userAdmin['password'])) {
-                $token = sha1(''.$userAdmin['username'].$userAdmin['password'].time());
-                $_SESSION['token'] = $token;
-                $this->AdministratorsModel->insertToken($token, $userAdmin['username']);
+                session_start();
+                $tokenAdmin = sha1(''.$userAdmin['username'].$userAdmin['password'].time());
+                $_SESSION['tokenAdmin'] = $tokenAdmin;
                 header("Location: /dashboard/admin");
             } else {
                 $message = "Đăng nhập không thành công !";
