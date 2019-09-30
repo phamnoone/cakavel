6ba8124fd4e3fe0a1841b0f49fba837377902aa4
= <=>
2019-09-30 16:48:22 +0700

    update load chậm

--- app/views/template/login/header.php
+++ app/views/template/login/header.php
@@ -6,9 +6,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Đăng nhập</title>
-  <link rel="icon" type="image/png" href="/public/images/icons/favicon.ico"/>
+  <link rel="icon" type="image/png" href="/public/images/icons/favicon.ico">
+  <link rel="stylesheet" href="/public/stylesheets/animate.min.css" >
+  <link rel="stylesheet" href="/public/stylesheets/custom.min.css" >
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link href="/public/stylesheets/animate.min.css" rel="stylesheet">
-  <link href="/public/stylesheets/custom.min.css" rel="stylesheet">
 </head>
 <body class="login">
