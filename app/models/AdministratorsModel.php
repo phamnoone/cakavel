8414cd408ad3abfd44b76da7e0c87d0dd1d1b46d
= <=>
2019-10-04 00:09:24 +0700

    update login with cookies

--- app/models/AdministratorsModel.php
+++ app/models/AdministratorsModel.php
@@ -15,6 +15,28 @@ class AdministratorsModel extends Model {
         }
        
     }
+
+    function insertToken($token, $username) {
+        $sql = "UPDATE administrators SET `token` = :token WHERE `administrators`.`username` = :username";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':token',$token);
+        $stmt->bindValue(':username',$username);
+        $stmt->execute();
+    }
+
+    function checkToken($token){
+        $sql = "SELECT * FROM administrators WHERE `administrators`.`token` = :token";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':token',$token);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
 
 ?>
