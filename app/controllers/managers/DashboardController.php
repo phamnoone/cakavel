24db4cd556bff3ee563408138767c2a163115b28
mdkoe <pham_hong_thanh@mediado.jp>
2019-10-17 01:22:06 +0900

    refactor model

--- app/controllers/managers/DashboardController.php
+++ app/controllers/managers/DashboardController.php
@@ -14,24 +14,7 @@ class DashboardController extends ManagersController
         $userProfile = $this->manager;
 
         if ($this->method === 'POST') {
-            $profileUpdate = $this->data;
-            if ($profileUpdate['nameprofile'] == $userProfile['name'] && $profileUpdate['description'] == $userProfile['note'] && empty($_FILES['image']['name'])) {
-                $message['update'] = 'Bạn chưa thay đổi thông tin !';
-            } else {
-
                 //REQUIRE viết lại đoạn update
-                if (empty($_FILES['image']['name'])) {
-                    $_FILES['image']['name'] = $userProfile['image'];
-                }
-                $image = new UploadImgHelper('image');
-                $image->upLoadFile();
-                $message['image'] = $image->messimg;
-                if ($this->AdministratorsModel->updateInfor($profileUpdate['nameprofile'], $_FILES['image']['name'], $profileUpdate['description'], $_SESSION['username'])) {
-                    $message['update'] = '';
-                    $this->redriect("/managers/dashboard/admin");
-                } else {
-                    $message['update'] = 'Cập nhật không thành công !';
-                }
             }
         }
         $this->view('managers/admin', [
