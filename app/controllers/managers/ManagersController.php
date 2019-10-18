604b1a7e5a491981d4aba158e717788ff039eb0f
mdkoe <pham_hong_thanh@mediado.jp>
2019-10-18 12:21:23 +0900

    refactor code

--- app/controllers/managers/ManagersController.php
+++ app/controllers/managers/ManagersController.php
@@ -3,6 +3,7 @@ class ManagersController extends Controller
 {
     const TOKEN_KEY = 'token_admin';
     const REDRIECT_URL = '/managers/auth/login';
+    
     public $isLoginPage = false;
     public $manager = null;
 
@@ -12,9 +13,9 @@ class ManagersController extends Controller
 
         if (empty($_SESSION[self::TOKEN_KEY]) && !$this->AdministratorsModel->checkAuthenWithToken($_SESSION[self::TOKEN_KEY]) && !$this->isLoginPage) {
             $this->redirect(self::REDRIECT_URL);
-        } else {
-            $this->manager = $this->AdministratorsModel->getWithToken($_SESSION[self::TOKEN_KEY]);
         }
+
+        $this->manager = $this->AdministratorsModel->getWithToken($_SESSION[self::TOKEN_KEY]);
 
         $this->view('template/managers/header', [
             'manager' => $this->manager
