82cde7ed13adedbbe1df84845962eec595fc911b
= <=>
2019-10-19 20:40:21 +0700

    upadte 4 mat

--- app/controllers/managers/DashboardController.php
+++ app/controllers/managers/DashboardController.php
@@ -16,20 +16,20 @@ class DashboardController extends ManagersController
             if ($userUpdate['nameprofile'] == $userProfile['name'] && $userUpdate['description'] == $userProfile['note'] && empty($_FILES['image']['name']) ){
                 $message['update'] = 'Bạn chưa thay đổi thông tin !';
             } else {
-                  if (empty($_FILES['image']['name'])) {
-                      $_FILES['image']['name'] = $userProfile['image'];
-                  }
-                  $userProfile['name'] = $userUpdate['nameprofile'];
-                  $userProfile['note'] = $userUpdate['description'];
-                  $userProfile['image'] = $_FILES['image']['name'];
-                  $uploadImage = new UploadImgHelper('image');
-                  $uploadImage->upLoadFile();
-                  $message['update'] = $uploadImage->messimg;
-                  if ($this->AdministratorsModel->updateInfor($userProfile)) {
-                      $message['update'] = 'Cập nhật thành công !';
-                  } else {
-                        $message['update'] = 'Cập nhật không thành công !';
-                  }
+                    if (empty($_FILES['image']['name'])) {
+                        $_FILES['image']['name'] = $userProfile['image'];
+                    }
+                    $userProfile['name'] = $userUpdate['nameprofile'];
+                    $userProfile['note'] = $userUpdate['description'];
+                    $userProfile['image'] = $_FILES['image']['name'];
+                    $uploadImage = new UploadImgHelper('image');
+                    $uploadImage->upLoadFile();
+                    $message['update'] = $uploadImage->messimg;
+                    if ($this->AdministratorsModel->updateInfor($userProfile)) {
+                          $message['update'] = 'Cập nhật thành công !';
+                    } else {
+                          $message['update'] = 'Cập nhật không thành công !';
+                    }
             }
         }
 
